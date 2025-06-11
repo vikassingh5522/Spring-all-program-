393671e106c0e08320c1c43f279201c638fea056
vikas singh <135737417+vikassingh5522@users.noreply.github.com>
2025-06-11 17:58:45 +0530

    Add files via upload

---
+++ SpringConf/src/main/java/org/example/MyBox.java
@@ -0,0 +1,59 @@
+package org.example;
+
+
+
+public class MyBox {
+    private double len;
+    private double wid;
+    private double hig;
+
+    // ------- Constructors ---------
+    public MyBox() {
+        len = 1;
+        wid = 1;
+        hig = 1;
+    }
+
+    public MyBox(double t) {
+        len = t;
+        wid = t;
+        hig = t;
+    }
+
+    public MyBox(double a, double b, double c) {
+        len = a;
+        wid = b;
+        hig = c;
+    }
+
+    // ----------- Getter Methods ------------
+    public double getLen() {
+        return len;
+    }
+
+    public double getWid() {
+        return wid;
+    }
+
+    public double getHig() {
+        return hig;
+    }
+
+    // ----------- Setter Methods ------------
+    public void setLen(double len) {
+        this.len = len;
+    }
+
+    public void setWid(double wid) {
+        this.wid = wid;
+    }
+
+    public void setHig(double hig) {
+        this.hig = hig;
+    }
+
+    // ----------- Volume Method ------------
+    public void volume() {
+        System.out.println("Box volume: " + (len * wid * hig));
+    }
+}
